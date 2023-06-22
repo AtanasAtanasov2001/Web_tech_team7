51c74b3ee03d0facf0f308e96f9cb60c45d9edeb
specific20 <peczem625@gmail.com>
2023-06-22 15:25:55 +0300

    Add registration form

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,20 +2,40 @@ import React, { useState } from "react";
 import { Container } from "@mui/material";
 import Game from "./Game";
 import LoginForm from "./LoginForm";
-import "./App.css"; 
+import RegistrationForm from "./RegistrationForm";
+import "./App.css";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [showRegistration, setShowRegistration] = useState(false);
 
   const handleLogin = (username, password) => {
-    
     setLoggedIn(true);
+  };
+
+  const handleRegistration = (username, password) => {
+    // Handle registration logic
+    setLoggedIn(true);
+    setShowRegistration(false);
   };
 
   return (
     <Container>
-      <h1 className="login-form">Head 2 head Chess</h1>
-      {loggedIn ? <Game /> : <LoginForm handleLogin={handleLogin} />}
+      <h1 className="login-form">Head 2 Head Chess</h1>
+      {loggedIn ? (
+        <Game />
+      ) : (
+        <>
+          {showRegistration ? (
+            <RegistrationForm handleRegistration={handleRegistration} />
+          ) : (
+            <LoginForm handleLogin={handleLogin} />
+          )}
+          <button onClick={() => setShowRegistration(!showRegistration)}>
+            {showRegistration ? "Back to Login" : "Register"}
+          </button>
+        </>
+      )}
     </Container>
   );
 };
