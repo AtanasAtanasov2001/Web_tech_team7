23eef36115ead84c946f37684916a19ffb14e5e9
Boyan Donchev <boyan.donchev@wolfpack-dcs.com>
2023-06-22 23:16:45 +0300

    login regiser functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@mui/material";
 import Game from "./Game";
 import LoginForm from "./LoginForm";
@@ -10,24 +10,39 @@ import axios from "axios";
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showRegistration, setShowRegistration] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  useEffect(() => {
+    if(token) {
+      setLoggedIn(true);
+    }
+  }, [token])
 
   const handleLogin = (username, password) => {
-    setLoggedIn(true);
+    axios.post("http://localhost:4000/auth/login", { username, password },{ headers: { 'Content-Type': 'application/json' } })
+      .then(r => {
+        if (r.status === 200) {
+          localStorage.setItem("token", "5ZvqN3FRsopaOUrSbXbNtyOIr1DVxr88kxEbAs72Hpw7krgWyjo5VmE4YiBIQ9aGN1TO7hQKkXZ4MJPQSo9a35ml8MmO8usMopNugu8xhHznPsz0iGqszdBCu6JnIx8JeSxsQh031lSwOUwzfnQr0DsXLI8AR4RZPDiXmNqCbVU" + r.data.token);
+          setLoggedIn(true);
+          alert("Success");
+        }
+      })
+      .catch(e => {
+        alert(e.response.data)
+      })
   };
 
   const handleRegistration = (username, password) => {
-    setLoggedIn(true);
-    setShowRegistration(false);
-
-    axios.post("http://localhost:4000/auth/mockRegister", {
-      username: "111",
-      password: "111"
-    }, { headers: { 'Content-Type': 'application/json' } }).then(r => 
-      { console.log(r) 
-      
-      
+    axios.post("http://localhost:4000/auth/register", { username, password },{ headers: { 'Content-Type': 'application/json' } })
+      .then(r => {
+        if (r.status === 200) {
+          alert("Success");
+          setShowRegistration(false);
+        }
       })
-
+      .catch(e => {
+        alert(e.response.data);
+      });
   };
 
   return (
